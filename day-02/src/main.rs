db736f214e97e896633f83d2e9f7c22585268ddd
nextuponstream <nextuponstream@gmail.com>
2024-12-02 18:14:20 +0100

    WIP

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -3,7 +3,8 @@ use day_02::Report;
 fn main() {
     let input = std::fs::read_to_string("./day-02/input").unwrap();
 
-    let lines: Vec<&str> = input.split("\n").collect();
+    let mut lines: Vec<&str> = input.split("\n").collect();
+    lines.pop(); // delete last empty line
     let reports: Vec<Report> = lines.iter().map(|line| (*line).try_into().unwrap()).collect();
     let safe_count = reports.iter().filter(|r| r.safe()).count();
     println!("{safe_count}")
